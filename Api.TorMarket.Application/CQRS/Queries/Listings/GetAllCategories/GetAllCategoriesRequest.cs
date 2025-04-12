5bcfe82e88544c4b22908bf4274a5d5d70f36b14
ckalmar <ckalmar@craneware.com>
2025-04-12 17:01:41 +0100

    Listings controller update

---
+++ Api.TorMarket.Application/CQRS/Queries/Listings/GetAllCategories/GetAllCategoriesRequest.cs
@@ -0,0 +1,8 @@
+﻿using Api.TorMarket.Domain.Models;
+using MediatR;
+
+namespace Api.TorMarket.Application.CQRS.Queries.Listings.GetAllCategories;
+
+public sealed record GetAllCategoriesRequest : IRequest<IEnumerable<ListingCategory>>
+{
+}
--- Api.TorMarket.Persistence/Repositories/ListingCategoryRepository.cs
+++ Api.TorMarket.Persistence/Repositories/ListingCategoryRepository.cs
@@ -10,16 +10,22 @@ internal class ListingCategoryRepository(
     IApplicationDbContext context
 ) : IListingCategoryRepository
 {
+    public async Task<List<ListingCategory>> GetAllAsync(
+        CancellationToken cancellationToken
+    ) =>
+        await context.ListingCategory
+            .AsNoTracking()
+            .Select(x => x.ToModel()!)
+            .ToListAsync(cancellationToken);
+
     public async Task<ListingCategory?> GetByNameAsync(
         string name,
         CancellationToken cancellationToken
-    )
-    {
-        var category = await context.ListingCategory.FirstOrDefaultAsync(
-            category => category.Name == name,
-            cancellationToken
-        );
-
-        return category?.ToModel();
-    }
+    ) =>
+        (
+            await context.ListingCategory.FirstOrDefaultAsync(
+                category => category.Name == name,
+                cancellationToken
+            )
+        )?.ToModel();
 }
