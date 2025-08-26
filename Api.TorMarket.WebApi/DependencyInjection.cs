b5e4a240549b521c870376f7e74e635ed8907c79
ckalmar <ckalmar@craneware.com>
2025-08-26 10:55:48 +0100

    Program.cs cleanup

---
+++ Api.TorMarket.WebApi/DependencyInjection.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Api.TorMarket.WebApi;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddWebApiDependencies(
+        this IServiceCollection services
+    ) =>
+        services
+            .AddCors()
+            .AddAuthentication();
+
+    private static IServiceCollection AddCors(
+        this IServiceCollection services
+    ) =>
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(corsBuilder =>
+            {
+                corsBuilder
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+        });
+
+    private static IServiceCollection AddAuthentication(
+        this IServiceCollection services
+    )
+    {
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(
+                JwtBearerDefaults.AuthenticationScheme,
+                options =>
+                {
+                    options.Authority = "https://tormarket.us.auth0.com/";
+                    options.Audience = "https://tormarket.com/api";
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = "name",
+                        RoleClaimType = "permissions",
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                    };
+                }
+            );
+
+        return services;
+    }
+}
--- Api.TorMarket.WebApi/Program.cs
+++ Api.TorMarket.WebApi/Program.cs
@@ -1,23 +1,30 @@
 using Api.TorMarket.Application;
 using Api.TorMarket.Infrastructure;
 using Api.TorMarket.Persistence;
+using Api.TorMarket.WebApi;
 using Asp.Versioning;
 using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services
+    .AddApplicationDependencies()
+    .AddInfrastructureDependencies(builder.Configuration)
+    .AddPersistenceDependencies(builder.Configuration)
+    .AddWebApiDependencies()
+    .AddAuthorization()
+    .AddControllers();
 
 builder.Services
     .AddEndpointsApiExplorer()
     .AddSwaggerGen(options =>
     {
-        options.SwaggerDoc("v1", 
+        options.SwaggerDoc(
+            "v1",
             new OpenApiInfo
             {
-                Title = "Api.TorMarket", 
+                Title = "Api.TorMarket",
                 Version = "v1"
             }
         );
@@ -35,48 +42,6 @@ builder.Services
     {
         options.GroupNameFormat = "'v'V";
         options.SubstituteApiVersionInUrl = true;
-    });
-
-builder.Services
-    .AddCors(options =>
-    {
-        options.AddDefaultPolicy(corsBuilder =>
-        {
-            corsBuilder
-                .AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
-    });
-
-//TODO - Load from configuration
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(
-        JwtBearerDefaults.AuthenticationScheme, 
-        options =>
-        {
-            options.Authority = "https://tormarket.us.auth0.com/";
-            options.Audience = "https://tormarket.com/api";
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-            };
-        }
-    );
-
-builder.Services.AddAuthorization();
-
-builder.Services
-    .AddApplicationDependencies()
-    .AddInfrastructureDependencies(builder.Configuration)
-    .AddPersistenceDependencies(builder.Configuration)
-    .AddControllers(options =>
-    {
-        options.SuppressAsyncSuffixInActionNames = false;
     });
 
 var app = builder.Build();
