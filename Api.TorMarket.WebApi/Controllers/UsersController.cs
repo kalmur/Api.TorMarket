a9defeb668720cac75922f00b9c15fb2b02b7720
ckalmar <ckalmar@craneware.com>
2025-09-02 12:27:50 +0100

    UsersController format cleanup

--- Api.TorMarket.WebApi/Controllers/UsersController.cs
+++ Api.TorMarket.WebApi/Controllers/UsersController.cs
@@ -66,8 +66,8 @@ public sealed class UsersController : ControllerBase
             );
     }
 
-    [Authorize("admin")]
     [HttpGet]
+    [Authorize("admin")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<UserDto>))]
     public async Task<IActionResult> GetAllAsync(
         [FromServices] IQueryHandler<GetAllUsersQuery, IEnumerable<User?>> mediator,
